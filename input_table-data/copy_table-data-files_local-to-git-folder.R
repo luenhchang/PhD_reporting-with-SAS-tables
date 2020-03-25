9219b4ca85fdc020f3cf562c26ab6feec95889a9
luenhchang <luenhchang@gmail.com>
2020-03-26 08:44:57 +1000

    data files for chapter 3

--- input_table-data/copy_table-data-files_local-to-git-folder.R
+++ input_table-data/copy_table-data-files_local-to-git-folder.R
@@ -2,9 +2,9 @@
 # Folder locations
 dir.local.main <- "D:/Now/library_genetics_epidemiology/"
 dir.chapter2 <- paste0(dir.local.main,'slave_NU/NU_data_processed_exports/')
+dir.chapter3 <- paste0(dir.local.main,'GWAS/PRS_UKB_201711/')
 
 dir.git.input <- "D:/git/PhD_reporting-with-SAS-tables_thesis-supplementary-tables/input_table-data/"
-
 
 #---------------------------------------------------------------------
 # Copy supplementary table data files from thesis chapter 2
@@ -42,4 +42,45 @@ for (i in 1:length(file.path.s2.source)){
   print(paste0("Copying file from ", source.file.path, " to ", destin.file.path))
   file.copy(source.file.path,destin.file.path, overwrite = TRUE)
 }
+
+#---------------------------------------------------------------------
+# Copy supplementary table data files from thesis chapter 3
+#---------------------------------------------------------------------
+file.s3_1.source <- paste0(dir.chapter3,'LDBasedClumping/output/','num-SNPs-by-CHR-meet-8-p-thresholds_QCed-GSCAN-GWAS_LD-clumping.csv')
+file.s3_1.destin <- paste0(dir.git.input,basename(file.s3_1.source))
+
+file.s3_2.source <- paste0(dir.chapter3,'phenotypeData/','numb-target-pheno-GSCAN-PRS-associ_survived-5-signi-thresholds_manu2-QIMR19Up-everDrug1to9-AUD-CUD_sex-PRS-int-exclu_all-sex-groups.tsv')
+file.s3_2.destin <- paste0(dir.git.input,basename(file.s3_2.source))
+
+file.s3_3.source <- paste0(dir.chapter3,"GCTA/output_tabulated/","GCTA_wide-format_fixed-eff_SE_R2_GSCAN-PRS_QIMR-19up_everUsing10drugs-diagAU-diagCU.tsv")
+file.s3_3.destin <- paste0(dir.git.input,basename(file.s3_3.source))
+
+file.s3_4.source <- paste0(dir.chapter3,"phenotypic-correlations/","phenotypic-corr-dataframe-between-GSCAN-PRS-and-targ-phenotypes-manu2-QIMR19Up-all-sex-groups.tsv")
+file.s3_4.destin <- paste0(dir.git.input,basename(file.s3_4.source))
+
+file.path.s3.source <- c(file.s3_1.source,
+                         file.s3_2.source,
+                         file.s3_3.source,
+                         file.s3_4.source)
+
+file.path.s3.destin <- c(file.s3_1.destin,
+                         file.s3_2.destin,
+                         file.s3_3.destin,
+                         file.s3_4.destin)
+
+for (i in 1:length(file.path.s3.source)){
+  source.file.path <- file.path.s3.source[i]
+  destin.file.path <- file.path.s3.destin[i]
+  print(paste0("Copying file from ", source.file.path, " to ", destin.file.path))
+  file.copy(source.file.path,destin.file.path, overwrite = TRUE)
+}
+
+
+
+
+file.s4_.source <- paste0(dir.chapter4,)
+file.s4_.destin <- paste0(dir.git.input,basename(file.s4_.source))
+
+
+
 
