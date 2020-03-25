d95cfe33a23182897328dcb729e5cf845b8ca0e0
luenhchang <luenhchang@gmail.com>
2020-03-26 09:36:40 +1000

    Add table data files from Chapter 4

--- input_table-data/copy_table-data-files_local-to-git-folder.R
+++ input_table-data/copy_table-data-files_local-to-git-folder.R
@@ -3,6 +3,7 @@
 dir.local.main <- "D:/Now/library_genetics_epidemiology/"
 dir.chapter2 <- paste0(dir.local.main,'slave_NU/NU_data_processed_exports/')
 dir.chapter3 <- paste0(dir.local.main,'GWAS/PRS_UKB_201711/')
+dir.chapter4 <- paste0(dir.local.main,'GWAS/PRS_UKB_201711/')
 
 dir.git.input <- "D:/git/PhD_reporting-with-SAS-tables_thesis-supplementary-tables/input_table-data/"
 
@@ -75,11 +76,41 @@ for (i in 1:length(file.path.s3.source)){
   file.copy(source.file.path,destin.file.path, overwrite = TRUE)
 }
 
+#---------------------------------------------------------------------
+# Copy supplementary table data files from thesis chapter 4
+#---------------------------------------------------------------------
+file.s4_1.source <- paste0(dir.chapter4,"phenotypeData/","numb-target-pheno-GSCAN-PRS-associ_survived-5-signi-thresholds_manu3-QIMR-adults-aged20to90_GSCAN-phenotypes_9-diagnoses_sex-PRS-int-inclu.tsv")
+file.s4_1.destin <- paste0(dir.git.input,basename(file.s4_1.source))
+
+
+file.s4_2.source <- paste0(dir.chapter4,"GCTA/output_tabulated/","GCTA_wide-format_fixed-effect-of_PRS_sex-PRS_on_QIMR-adults-aged-20-90_GSCAN-phenotypes_nicotine-alcohol-dependence-and-more_sex-PRS-int-included-in-models.tsv")
+file.s4_2.destin <- paste0(dir.git.input,basename(file.s4_2.source))
+
+file.s4_3.source <- paste0(dir.chapter4,"phenotypeData/","numb-target-pheno-GSCAN-PRS-associ_survived-5-signi-thresholds_manu3-QIMR-adults-aged-20-90-GSCAN-phenotypes_nicotine-alcohol-dependence-and-more_sex-PRS-int-exclu_all-sex-groups.tsv") # This file name is too long to copy
+file.s4_3.destin <- paste0(dir.git.input,"Ch4_table-Sup03_file-renamed.tsv")
+
+file.s4_4.source <- paste0(dir.chapter4,"GCTA/output_tabulated/","GCTA_wide-format_fixed-eff_SE_R2_GSCAN-PRS_QIMR-adults-aged-20-90_GSCAN-phenotypes_nicotine-alcohol-dependence-and-more.tsv")
+file.s4_4.destin <- paste0(dir.git.input,basename(file.s4_4.source))
+
+file.path.s4.source <- c(file.s4_1.source,
+                         file.s4_2.source,
+                         file.s4_3.source,
+                         file.s4_4.source)
+
+file.path.s4.destin <- c(file.s4_1.destin,
+                         file.s4_2.destin,
+                         file.s4_3.destin,
+                         file.s4_4.destin)
+
+for (i in 1:length(file.path.s4.source)){
+  source.file.path <- file.path.s4.source[i]
+  destin.file.path <- file.path.s4.destin[i]
+  print(paste0("Copying file from ", source.file.path, " to ", destin.file.path))
+  file.copy(source.file.path,destin.file.path, overwrite = TRUE)
+}
 
 
 
-file.s4_.source <- paste0(dir.chapter4,)
-file.s4_.destin <- paste0(dir.git.input,basename(file.s4_.source))
 
 
 
